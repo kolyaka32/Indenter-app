e87a7a79eba78e24e3377d34bd518c7ffb655de6
kolyaka32 <nik.kazankov.05@mail.ru>
2026-03-29 00:53:43 +0300

    Updated image and save part

---
+++ create-data-file.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\7-Zip\7z.exe" a -tzip build/data-chess.dat ani/* fnt/* img/* mus/* snd/* -mmt -mx9 -ssw
---
+++ create-data-file.sh
@@ -0,0 +1,1 @@
+zip -9ru build/data-chess.dat ani/* img/* mus/* snd/* fnt/*
